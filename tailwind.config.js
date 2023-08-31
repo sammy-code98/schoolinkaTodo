22078fcda964fbf8cee473c152158f73feb27b41
sammy-code98 <sammysmart98@yahoo.com>
2023-08-31 06:09:39 +0100

    feat:configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        base_white: "#FFFFFF",
+        primary_grey: "#344054",
+        primary_black: "#101828",
+        primary_blue: "#3F5BF6",
+      },
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
